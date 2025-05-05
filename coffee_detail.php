7211384e6b90b7872af2a9abd90897c0465ee791
Özgür KILIÇ <ozgurkilic147@gmail.com>
2025-05-05 11:56:43 +0200

    update

--- coffee_detail.php
+++ coffee_detail.php
@@ -1,5 +1,13 @@
 <?php
+
 session_start();
+
+if (!isset($_SESSION['client_logged_in']) || $_SESSION['client_logged_in'] !== true) {
+    $coffeeType = $_GET['coffee'] ?? '';
+    header("Location: clientlogin.php?redirect=coffee_detail.php&coffee=" . urlencode($coffeeType));
+    exit();
+}
+
 
 // If the client is not logged in, redirect to login page with redirect back info
 if (!isset($_SESSION['client_logged_in']) || $_SESSION['client_logged_in'] !== true) {
