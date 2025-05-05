384eec46b64220a34468756beae3b6df3b053eba
Özgür KILIÇ <ozgurkilic147@gmail.com>
2025-05-05 10:55:34 +0200

    LoginError

--- clientprofile.php
+++ clientprofile.php
@@ -1,19 +1,24 @@
 <?php
 session_start();
-if (!isset($_SESSION['client_logged_in']) || $_SESSION['client_logged_in'] !== true) {
-    header("Location: clientlogin.php");
-    exit();
-}
+
+include 'header.php';
 ?>
 
-<?php include 'header.php'; ?>
-<link rel="stylesheet" href="css/login.css"> <!-- Reuse same style -->
+<link rel="stylesheet" href="css/login.css">
 
 <div class="login-container">
     <div class="login-box">
-        <h2>Welcome, <?php echo htmlspecialchars($_SESSION['client_username']); ?>!</h2>
+        <h2>
+            Welcome,<br>
+            <?php
+            if (isset($_SESSION['client_username'])) {
+                echo htmlspecialchars($_SESSION['client_username']);
+            } else {
+                echo "<span style='color:red'>[Unknown]</span>";
+            }
+            ?>
+        </h2>
         <p>You are successfully logged in as a client.</p>
-        <br>
         <form action="clientlogout.php" method="post">
             <button type="submit">Log Out</button>
         </form>
